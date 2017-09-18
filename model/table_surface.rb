5b70fbc9634aff153f0c13b27add24886d8cb8d6
Vinh To <vinhtt0305@gmail.com>
2017-09-18 19:19:53 +0900

    Change name of attr of TableSurface from 'length' to 'width'.

--- model/table_surface.rb
+++ model/table_surface.rb
@@ -1,25 +1,25 @@
 require_relative 'position'
 
 class TableSurface
-  attr_accessor :length, :height
+  attr_accessor :width, :height
 
-  def initialize(length, height)
-    validate!(length, height)
+  def initialize(width, height)
+    validate!(width, height)
 
-    @length = length
+    @width = width
     @height = height
   end
 
   def inside_table_surface?(position)
     raise ArgumentError.new('Position is invalid') unless position.is_a?(Position)
 
-    (0..(@length - 1)).include?(position.x) && (0..(@height - 1)).include?(position.y)
+    (0..(@width - 1)).include?(position.x) && (0..(@height - 1)).include?(position.y)
   end
 
   private
 
-  def validate!(length, height)
-    if !length.is_a?(Integer) || length < 0
+  def validate!(width, height)
+    if !width.is_a?(Integer) || width < 0
       raise ArgumentError.new('Length must be an integer and greater than or equal to 0')
     end
 
