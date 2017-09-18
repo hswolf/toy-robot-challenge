96f4de5559c5af23dfcd64362747e266cc54a81d
Vinh To <vinhtt0305@gmail.com>
2017-09-18 19:14:04 +0900

    Add validation for TableSurface model.

--- model/table_surface.rb
+++ model/table_surface.rb
@@ -14,5 +14,12 @@ class TableSurface
   private
 
   def validate!(length, height)
+    if !length.is_a?(Integer) || length < 0
+      raise ArgumentError.new('Length must be an integer and greater than or equal to 0')
+    end
+
+    if !height.is_a?(Integer) || height < 0
+      raise ArgumentError.new('Height must be an integer and greater than or equal to 0')
+    end
   end
 end
